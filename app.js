a64ca6b4e5926bba3bb27f653b581b6f966fd30c
evasquez11 <ethan.f.vasquez-1@ou.edu>
2023-11-18 15:53:24 -0600

    incorporate i2c functionality

--- app.js
+++ app.js
@@ -1,32 +1,60 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const Gpio = require('./mock-gpio').Gpio; // Using mock GPIO
-// const Gpio = require('onoff').Gpio; // Real GPIO
+
+// Determine if running on Raspberry Pi
+const isRaspberryPi = false; // Change this based on your environment detection logic
+
+// Conditional imports for GPIO
+const Gpio = isRaspberryPi ? require('onoff').Gpio : require('./mock-gpio').Gpio;
+
+// Conditional imports for I2C
+const I2C = isRaspberryPi ? require('real-i2c-library') : require('./mock-i2c');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public')); // Serve static files
+app.use(express.static('public')); // Serve static files from 'public' directory
 
 io.on('connection', (socket) => {
-    const sensor = new Gpio(17, 'in', 'both'); // Mock GPIO pin
-
-    sensor.watch((err, value) => {
+    // Example GPIO sensor
+    const gpioSensor = new Gpio(17, 'in', 'both'); // Using GPIO pin 17 as an example
+    gpioSensor.watch((err, value) => {
         if (err) {
-            console.error('Error:', err);
+            console.error('GPIO Error:', err);
         } else {
             socket.emit('gpioData', value);
         }
     });
 
+// Inside io.on('connection', ...)
+const i2cSensor = new I2C(0x00, 1); // Using mock I2C
+
+setInterval(async () => {
+    const magneticStrength = await i2cSensor.readSensorData();
+    // console.log("Emitting magnetic strength:", magneticStrength); // Check the output
+    socket.emit('i2cData', { magneticStrength });
+}, 2000);
+
+
+
     socket.on('disconnect', () => {
-        sensor.unexport();
         console.log('Client disconnected');
+        if (!isRaspberryPi) {
+            gpioSensor.unexport();
+        }
     });
 });
 
 server.listen(3000, () => {
     console.log('Server listening on port 3000');
+});
+
+// Cleanup on exit
+process.on('SIGINT', () => {
+    if (!isRaspberryPi) {
+        gpioSensor.unexport();
+    }
+    process.exit();
 });
--- public/sensor-data.js
+++ public/sensor-data.js
@@ -1,13 +1,29 @@
 const socket = io.connect();
 
-socket.on('gpioData', (data) => {
-    console.log('Received GPIO data:', data);
-    // Update the web page with new data
-    updateSensorDisplay(data);
+// Handle incoming GPIO data
+socket.on('gpioData', (gpioValue) => {
+    console.log('Received GPIO data:', gpioValue);
+    updateGpioDisplay(gpioValue);
 });
 
-function updateSensorDisplay(data) {
-    // This function updates the web page based on sensor data
-    const sensorDisplayElement = document.getElementById('sensorDisplay');
-    sensorDisplayElement.textContent = `Sensor Value: ${data}`;
+// Handle incoming I2C data
+socket.on('i2cData', (data) => {
+    console.log('Received I2C data:', data); // Check if data is received
+    updateI2CDisplay(data);
+});
+
+function updateGpioDisplay(value) {
+    // Update the GPIO sensor display on the webpage
+    const gpioDisplayElement = document.getElementById('gpioDisplay');
+    if (gpioDisplayElement) {
+        gpioDisplayElement.textContent = `GPIO Sensor Value: ${value}`;
+    }
 }
+
+function updateI2CDisplay(data) {
+    const i2cDisplayElement = document.getElementById('i2cDisplay');
+    if (i2cDisplayElement) {
+        i2cDisplayElement.textContent = `Magnetic Field Strength: ${data.magneticStrength.toFixed(2)}`;
+    }
+}
+
