0926b93e9be05a635fe58fe8916333b90604bfe5
evasquez11 <ethan.f.vasquez-1@ou.edu>
2023-12-03 16:11:33 -0600

    turn LED on

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const isRaspberryPi = true; // Change this based on your environment detection l
 const Gpio = isRaspberryPi ? require('onoff').Gpio : require('./mock-gpio').Gpio;
 
 // Conditional imports for I2C
-const I2C = isRaspberryPi ? require('i2c-bus') : require('./mock-i2c');
+// const I2C = isRaspberryPi ? require('i2c-bus') : require('./mock-i2c');
 
-const RS485 = require(isRaspberryPi ? 'serialport' : './mock-rs485');
+// const RS485 = require(isRaspberryPi ? 'serialport' : './mock-rs485');
 
 
 const app = express();
@@ -29,40 +29,53 @@ io.on('connection', (socket) => {
     const gpioSensor2 = new Gpio(4, 'in', 'both'); // Use gpioSensor2 as Button (input)
 
 
-    gpioSensor1.watch((err, value) => {
-        if (err) console.error('GPIO Error:', err);
-        else socket.emit('gpioData1', value);
-    });
-
+    gpioSensor1.writeSync(1); // Turn LED on
+    setTimeout(() => gpioSensor1.writeSync(0), 2000); // Turn LED off after 2 seconds
+        
     gpioSensor2.watch((err, value) => {
-        if (err) console.error('GPIO Error:', err);
-        else socket.emit('gpioData2', value);
+        if (err) {
+            console.error('GPIO Error:', err);
+        } else {
+            gpioSensor1.writeSync(value); // Turn on/off LED based on button state
+            socket.emit('gpioData2', value); // Emit button state to client
+        }
     });
+    
+
+    // gpioSensor1.watch((err, value) => {
+    //     if (err) console.error('GPIO Error:', err);
+    //     else socket.emit('gpioData1', value);
+    // });
+
+    // gpioSensor2.watch((err, value) => {
+    //     if (err) console.error('GPIO Error:', err);
+    //     else socket.emit('gpioData2', value);
+    // });
 
     // Example I2C sensor setup
-    const i2cSensor1 = new I2C(0x00, 1); // First I2C sensor
-    const i2cSensor2 = new I2C(0x01, 1); // Second I2C sensor
+    // const i2cSensor1 = new I2C(0x00, 1); // First I2C sensor
+    // const i2cSensor2 = new I2C(0x01, 1); // Second I2C sensor
     // Replace 0x00 and 0x01 with actual I2C addresses for real sensors
 
-    setInterval(async () => {
-        const magneticStrength1 = await i2cSensor1.readSensorData();
-        socket.emit('i2cData1', { magneticStrength: magneticStrength1 });
+    // setInterval(async () => {
+    //     const magneticStrength1 = await i2cSensor1.readSensorData();
+    //     socket.emit('i2cData1', { magneticStrength: magneticStrength1 });
 
-        const magneticStrength2 = await i2cSensor2.readSensorData();
-        socket.emit('i2cData2', { magneticStrength: magneticStrength2 });
-    }, 2000);
+    //     const magneticStrength2 = await i2cSensor2.readSensorData();
+    //     socket.emit('i2cData2', { magneticStrength: magneticStrength2 });
+    // }, 2000);
 
     // Example RS485 sensor setup
-    const rs485Sensor1 = new RS485(); // First RS485 sensor
-    const rs485Sensor2 = new RS485(); // Second RS485 sensor
+    // const rs485Sensor1 = new RS485(); // First RS485 sensor
+    // const rs485Sensor2 = new RS485(); // Second RS485 sensor
 
-    setInterval(async () => {
-        const distance1 = await rs485Sensor1.readDistance();
-        socket.emit('rs485Data1', { distance: distance1 });
+    // setInterval(async () => {
+    //     const distance1 = await rs485Sensor1.readDistance();
+    //     socket.emit('rs485Data1', { distance: distance1 });
 
-        const distance2 = await rs485Sensor2.readDistance();
-        socket.emit('rs485Data2', { distance: distance2 });
-    }, 1000);
+    //     const distance2 = await rs485Sensor2.readDistance();
+    //     socket.emit('rs485Data2', { distance: distance2 });
+    // }, 1000);
 
     socket.on('disconnect', () => {
         console.log('Client disconnected');
