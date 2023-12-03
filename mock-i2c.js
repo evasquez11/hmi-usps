f791090d344865df64c7ac4921a912173e4a662c
cama-0004 <jesus.1.camarena@ou.edu>
2023-12-03 00:13:15 -0600

    change for ada fruit library

--- mock-i2c.js
+++ mock-i2c.js
@@ -1,25 +1,26 @@
 // mock-i2c.js
+
+// adafruit-tlv493-i2c.js
+const { TLV493 } = require('adafruit-circuitpython-tlv493');
+
 class MockI2C {
     constructor(address, bus) {
-        this.address = address;
-        this.bus = bus;
-        console.log(`Mock I2C device at address ${address} on bus ${bus}`);
+        this.tlv493 = new TLV493({address, bus});
+        console.log(`Adafruit TLV493 device at address ${address} on bus ${bus}`);
     }
 
     // Simulate reading magnetic field strength
     readSensorData() {
-        return new Promise((resolve) => {
-            // Simulate magnetic field strength
-            //const magneticFieldStrength = Math.random() * 100; // Adjust the range as per your sensor's typical values
-            const x = Math.random() * 100; // Adjust the range as needed
-            const y = Math.random() * 100; // Adjust the range as needed
-            const z = Math.random() * 100; // Adjust the range as needed
-
-            setTimeout(() => resolve(({x,y,z})), 100);
+        return new Promise((resolve, reject) => {
+            try {
+                const data = this.tlv493.read();  // Adjust method based on the actual library
+                resolve(data);
+            } catch (error) {
+                reject(error);
+            }
         });
     }
-
-    // Add other methods if needed
 }
+    // Add other methods if needed
 
 module.exports = MockI2C;
