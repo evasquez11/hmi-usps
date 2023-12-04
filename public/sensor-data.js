42fbde3a9d41b707a1ef77cc40466269fc6ec619
evasquez11 <ethan.f.vasquez-1@ou.edu>
2023-12-03 21:49:23 -0600

    magnetic sensor implementation chart

--- public/sensor-data.js
+++ public/sensor-data.js
@@ -4,18 +4,16 @@ const socket = io.connect();
 const gpioCtx1 = document.getElementById('gpioChart1').getContext('2d');
 const gpioCtx2 = document.getElementById('gpioChart2').getContext('2d');
 const i2cCtx1 = document.getElementById('i2cChart1').getContext('2d');
-const i2cCtx2 = document.getElementById('i2cChart2').getContext('2d');
 const rs485Ctx1 = document.getElementById('rs485Chart1').getContext('2d');
 const rs485Ctx2 = document.getElementById('rs485Chart2').getContext('2d');
 
 const gpioChart1 = createSensorChart(gpioCtx1, 'GPIO Sensor 1');
 const gpioChart2 = createSensorChart(gpioCtx2, 'GPIO Sensor 2');
-const i2cChart1 = createSensorChart(i2cCtx1, 'Magnetic Field Strength 1');
-const i2cChart2 = createSensorChart(i2cCtx2, 'Magnetic Field Strength 2');
+const magneticFieldChart = createMagneticFieldChart(i2cCtx1, 'Magnetic Field Strength');
 const rs485Chart1 = createSensorChart(rs485Ctx1, 'Distance Sensor 1');
 const rs485Chart2 = createSensorChart(rs485Ctx2, 'Distance Sensor 2');
 
-// Function to create a sensor chart
+// Function to create a generic sensor chart
 function createSensorChart(ctx, label) {
     return new Chart(ctx, {
         type: 'line',
@@ -39,6 +37,43 @@ function createSensorChart(ctx, label) {
     });
 }
 
+// Function to create a magnetic field chart with three datasets
+function createMagneticFieldChart(ctx, label) {
+    return new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: [],
+            datasets: [
+                {
+                    label: `${label} X`,
+                    data: [],
+                    borderColor: 'rgb(255, 99, 132)',
+                    borderWidth: 1
+                },
+                {
+                    label: `${label} Y`,
+                    data: [],
+                    borderColor: 'rgb(54, 162, 235)',
+                    borderWidth: 1
+                },
+                {
+                    label: `${label} Z`,
+                    data: [],
+                    borderColor: 'rgb(75, 192, 192)',
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
 // Function to update a chart with new data
 function updateChart(chart, value) {
     const now = new Date();
@@ -51,10 +86,28 @@ function updateChart(chart, value) {
     chart.update();
 }
 
+// Function to update a magnetic field chart with new data
+function updateMagneticFieldChart(chart, data) {
+    const now = new Date().toLocaleTimeString();
+    chart.data.labels.push(now);
+
+    chart.data.datasets[0].data.push(data.Bx);
+    chart.data.datasets[1].data.push(data.By);
+    chart.data.datasets[2].data.push(data.Bz);
+
+    if (chart.data.labels.length > 20) { // Limit number of data points
+        chart.data.labels.shift();
+        chart.data.datasets.forEach(dataset => {
+            dataset.data.shift();
+        });
+    }
+
+    chart.update();
+}
+
 // WebSocket event listeners for sensor data
 socket.on('gpioData1', (value) => updateChart(gpioChart1, value));
 socket.on('gpioData2', (value) => updateChart(gpioChart2, value));
-socket.on('i2cData1', (data) => updateChart(i2cChart1, data.magneticStrength));
-socket.on('i2cData2', (data) => updateChart(i2cChart2, data.magneticStrength));
+socket.on('i2cData1', (data) => updateMagneticFieldChart(magneticFieldChart, data));
 socket.on('rs485Data1', (data) => updateChart(rs485Chart1, data.distance));
 socket.on('rs485Data2', (data) => updateChart(rs485Chart2, data.distance));
