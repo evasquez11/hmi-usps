2fdd8661e3804a9c10cf81d58a7aa56041c39a4d
evasquez11 <ethan.f.vasquez-1@ou.edu>
2023-12-06 01:19:23 -0600

    comment out function

--- app.js
+++ app.js
@@ -20,43 +20,43 @@ io.on('connection', (socket) => {
     const gpioSensor3 = new Gpio(6, 'out'); // New LED on GPIO 6
     const gpioSensor4 = new Gpio(5, 'in', 'both'); // New switch (input) on GPIO 5
 
-    let actuatorActivated = false;
+    // let actuatorActivated = false;
 
-    // Function to handle miss-sort detection
-    function checkForMissSort(binSensorValue) {
-        if (actuatorActivated && binSensorValue === 0) { // 0 represents inactive
-            console.log('Miss-sort detected: Actuator activated but no package detected.');
-            // You can also emit this event to the client if needed
-            socket.emit('missSort', 'Miss-sort detected');
-            actuatorActivated = false; // Reset the state
-        }
-    }
+    // // Function to handle miss-sort detection
+    // function checkForMissSort(binSensorValue) {
+    //     if (actuatorActivated && binSensorValue === 0) { // 0 represents inactive
+    //         console.log('Miss-sort detected: Actuator activated but no package detected.');
+    //         // You can also emit this event to the client if needed
+    //         socket.emit('missSort', 'Miss-sort detected');
+    //         actuatorActivated = false; // Reset the state
+    //     }
+    // }
 
     gpioSensor1.writeSync(1); // Turn LED on
 
-    gpioSensor2.watch((err, value) => {
-        if (err) {
-            console.error('GPIO Error:', err);
-        } else {
-            gpioSensor1.writeSync(value); // Turn on/off LED based on button state
-            socket.emit('gpioData2', value); // Emit button state to client
-            if (value === 1) { // 1 represents active
-                actuatorActivated = true; // Actuator is activated
-            }
-        }
-    });
+    // gpioSensor2.watch((err, value) => {
+    //     if (err) {
+    //         console.error('GPIO Error:', err);
+    //     } else {
+    //         gpioSensor1.writeSync(value); // Turn on/off LED based on button state
+    //         socket.emit('gpioData2', value); // Emit button state to client
+    //         if (value === 1) { // 1 represents active
+    //             actuatorActivated = true; // Actuator is activated
+    //         }
+    //     }
+    // });
 
     gpioSensor3.writeSync(1); // Turn new LED on
 
-    gpioSensor4.watch((err, value) => {
-        if (err) {
-            console.error('GPIO Error:', err);
-        } else {
-            gpioSensor3.writeSync(value); // Turn on/off the new LED based on the switch state
-            socket.emit('gpioData1', value); // Emit the switch state to the client
-            checkForMissSort(value);
-        }
-    });
+    // gpioSensor4.watch((err, value) => {
+    //     if (err) {
+    //         console.error('GPIO Error:', err);
+    //     } else {
+    //         gpioSensor3.writeSync(value); // Turn on/off the new LED based on the switch state
+    //         socket.emit('gpioData1', value); // Emit the switch state to the client
+    //         checkForMissSort(value);
+    //     }
+    // });
 
     socket.on('disconnect', () => {
         console.log('Client disconnected');
