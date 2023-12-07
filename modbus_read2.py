0018f5568ae8008f5e377895fd6048792e3ea8a1
cama-0004 <jesus.1.camarena@ou.edu>
2023-12-06 23:15:18 -0600

    modbus

--- modbus_read2.py
+++ modbus_read2.py
@@ -1,22 +1,39 @@
-from pymodbus.client import ModbusSerialClient as ModbusClient
-from pymodbus.exceptions import ModbusException
-import time
+from pymodbus.client.sync import ModbusSerialClient
 
-modbus_client = ModbusClient(method='rtu', port='/dev/ttyUSB0', baudrate=38400, parity='N', stopbits=1, bytesize=8, timeout=5)
+# Modbus communication parameters
+modbus_unit_id = 1
+modbus_port = '/dev/ttyUSB0'
+modbus_baudrate = 38400
+modbus_parity = 'N'
+modbus_stopbits = 1
+modbus_bytesize = 8
+modbus_timeout = 5
+
+# Create Modbus client
+modbus_client = ModbusSerialClient(
+    method='rtu',
+    port=modbus_port,
+    baudrate=modbus_baudrate,
+    parity=modbus_parity,
+    stopbits=modbus_stopbits,
+    bytesize=modbus_bytesize,
+    timeout=modbus_timeout
+)
 
 try:
+    # Connect to the Modbus device
     modbus_client.connect()
-    for attempt in range(5):
-        result = modbus_client.read_holding_registers(0, 1, unit=1)  # Read the first register
-        if not result.isError():
-            print("Register value:", result.registers[0])
-            break
-        else:
-            print("Read error, retrying...")
-        time.sleep(1)
-except ModbusException as e:
-    print(f"Modbus error: {e}")
+
+    # Read Analog Data output (Register 40001)
+    result = modbus_client.read_holding_registers(40001, 1, unit=modbus_unit_id)
+    if not result.isError():
+        analog_data_output = result.registers[0] / 1000.0  # Convert to mA
+        print(f"Analog Data Output: {analog_data_output} mA")
+    else:
+        print(f"Error reading Analog Data Output: {result}")
 except Exception as e:
-    print(f"General error: {e}")
+    print(f"Error: {e}")
 finally:
+    # Close the Modbus connection
     modbus_client.close()
+
