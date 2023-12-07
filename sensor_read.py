a8303cdd03d2e7fd643d435cdb2a75c1341179d7
cama-0004 <jesus.1.camarena@ou.edu>
2023-12-06 20:29:40 -0600

    address for sensors

--- sensor_read.py
+++ sensor_read.py
@@ -4,11 +4,11 @@ import adafruit_tlv493d
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 #Initialize the first sensor (Slave #0)
-tlv1 = adafruit_tlv493d.TLV493D(i2c, address=0x5e) 
+tlv1 = adafruit_tlv493d.TLV493D(i2c, address=0x1f) 
 
 
 # Initialize the second sensor (Slave #1)
-# tlv2 = adafruit_tlv493d.TLV493D(i2c, address=)
+tlv2 = adafruit_tlv493d.TLV493D(i2c, address=5e)
 
 while True:
     print("Sensor 1 - X: %s, Y: %s, Z: %s uT" % tlv1.magnetic)
