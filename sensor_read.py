dd9bf5aadd0efe54529d7343cb3a3e560a352543
cama-0004 <jesus.1.camarena@ou.edu>
2023-12-06 20:04:55 -0600

    sensor1 reading

--- sensor_read.py
+++ sensor_read.py
@@ -1,11 +1,15 @@
-import time
 import board
 import adafruit_tlv493d
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
-tlv = adafruit_tlv493d.TLV493D(i2c)
+
+tlv1 = adafruit_tlv493d.TLV493D(i2c, address=0x5e) // Initialize the first sensor (Slave #0)
+
+// Initialize the second sensor (Slave #1)
+// tlv2 = adafruit_tlv493d.TLV493D(i2c, address=)
 
 while True:
-    print("X: %s, Y:%s, Z:%s uT" % tlv.magnetic)
+    print("Sensor 1 - X: %s, Y: %s, Z: %s uT" % tlv1.magnetic)
+    print("Sensor 2 - X: %s, Y: %s, Z: %s uT" % tlv2.magnetic)
     time.sleep(1)
 
