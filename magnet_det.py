8b1e818355230fdbcd7bab13cc1aa53006e1af97
evasquez11 <ethan.f.vasquez-1@ou.edu>
2023-12-06 23:50:48 -0600

    starter magnet det file

---
+++ magnet_det.py
@@ -0,0 +1,48 @@
+import time
+import board
+import adafruit_mmc56x3
+import adafruit_tlv493d
+# import logging
+
+# Initialize logging
+# logging.basicConfig(filename='conveyor_belt_log.txt', level=logging.INFO,
+#                     format='%(asctime)s:%(levelname)s:%(message)s')
+
+# Initialize I2C and sensors
+i2c = board.I2C()
+mmc_sensor = adafruit_mmc56x3.MMC5603(i2c)
+tlv_sensor = adafruit_tlv493d.TLV493D(i2c)
+
+def detect_magnet(sensor, threshold):
+    x, y, z = sensor.magnetic
+    return abs(x) > threshold or abs(y) > threshold or abs(z) > threshold
+
+# Define thresholds for magnet detection and acceptable difference
+mmc_threshold = 50  # Replace with your calibrated threshold
+tlv_threshold = 50  # Replace with your calibrated threshold
+some_acceptable_difference = 0.5  # 0.5 seconds
+
+# Initialize variables to store last detection times and cycle times
+last_time_mmc = time.time()
+last_time_tlv = time.time()
+cycle_time_mmc = 0
+cycle_time_tlv = 0
+
+while True:
+    current_time = time.time()
+
+    if detect_magnet(mmc_sensor, mmc_threshold):
+        cycle_time_mmc = current_time - last_time_mmc
+        last_time_mmc = current_time
+        print(f"MMC Cycle Time: {cycle_time_mmc:.2f} seconds")
+
+    if detect_magnet(tlv_sensor, tlv_threshold):
+        cycle_time_tlv = current_time - last_time_tlv
+        last_time_tlv = current_time
+        print(f"TLV Cycle Time: {cycle_time_tlv:.2f} seconds")
+
+    # Comparing the cycle times
+    if abs(cycle_time_mmc - cycle_time_tlv) > some_acceptable_difference:
+        print("Potential belt stretch detected")
+
+    time.sleep(0.1)  # Adjust as needed
