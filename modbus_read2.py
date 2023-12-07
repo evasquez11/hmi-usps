9dcb85b0d95aba3bf1143aa136c8ba0bb4cd7af3
cama-0004 <jesus.1.camarena@ou.edu>
2023-12-07 01:48:51 -0600

    baud

--- modbus_read2.py
+++ modbus_read2.py
@@ -1,11 +1,12 @@
 import minimalmodbus
 
 instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)  # Replace 1 with your actual unit ID
-instrument.serial.baudrate = 19200
+instrument.serial.baudrate = 38400
 instrument.serial.parity = 'N'
 instrument.serial.stopbits = 1
 instrument.serial.bytesize = 8
 instrument.serial.timeout = 2  # Adjust as needed
+instrument.serial.rtscts = True
 
 try:
     # Read a register as a test
